ccf107519a47ba24badda1adc1e4f975dc1af3a7
Jose Gonzalez <gon20335@uvg.edu.gt>
2023-09-20 17:44:26 -0600

    modify sh

--- runParallel.sh
+++ runParallel.sh
@@ -3,3 +3,4 @@ g++ -c ./Parallel/Parallel.cpp -o ./Parallel/Parallel.o -fopenmp -lGL -lglut
 g++ -o ./Parallel/Parallel ./Parallel/Parallel.o ./Particle/Particle.o -fopenmp -lGL -lglut
 
 ./Parallel/Parallel 5000 1920 1080 1
+./Parallel/Parallel {num_particulas} {ancho} {alto} {radio_particula}
